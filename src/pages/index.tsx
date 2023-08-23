2c866a18066153e0fe14914d95b87baaf0a37f89
ehdrjs4502 <ehdrjs4502@naver.com>
2023-08-24 02:12:52 +0900

    index 페이지 ssg revalidate:20 으로 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -96,6 +96,6 @@ export async function getStaticProps() {
 
     return {
         props: { latestPostList, popularPostList, userIntroList },
-        revalidate: 20,
+        revalidate: 120,
     }
 }
