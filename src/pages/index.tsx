42392b744fe1250c117a4024e6f67f2422435a1c
ehdrjs4502 <ehdrjs4502@naver.com>
2023-08-24 17:22:10 +0900

    vercel 배포 테스트

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -76,8 +76,6 @@ export async function getStaticProps() {
     // //최신 게시글 목록 가져오기 (최대 6개 가져옴)
     const latestPostRes: any = await axios.get(`${API_URL}lists/posts?pageSize=6`)
 
-    console.log('데이터 바뀜', latestPostRes)
-
     //인기 게시글 목록 가져오기 (최대 3개 가져옴)
     const popularPostRes: any = await axios.get(`${API_URL}lists/posts?order=view&pageSize=3`)
 
