665747a7af2993f3e653ecd58f9fdca59921db00
ehdrjs4502 <ehdrjs4502@naver.com>
2023-08-24 15:55:37 +0900

    vercel 배포 테스트

--- src/pages/tag/index.tsx
+++ src/pages/tag/index.tsx
@@ -1,51 +1,53 @@
-// import HeadTitle from '@/components/HeadTitle'
-// import Navigation from '@/components/navigation/Navigation'
-// import { useRef } from 'react'
-// import axios from 'axios'
-// import TagList from '@/components/tag/TagList'
-// import TagHeader from '@/components/tag/TagHeader'
+import HeadTitle from '@/components/HeadTitle'
+import Navigation from '@/components/navigation/Navigation'
+import { useEffect, useRef } from 'react'
+import axios from 'axios'
+import TagList from '@/components/tag/TagList'
+import TagHeader from '@/components/tag/TagHeader'
 
-// type tag = {
-//     id: number
-//     name: string
-// }
+type tag = {
+    id: number
+    name: string
+}
 
-// interface Props {
-//     tagList: [tag]
-// }
+interface Props {
+    tagList: [tag]
+}
 
-// export default function Tag({ tagList }: Props) {
-//     const title = '태그 목록'
-//     const contentRef = useRef(null)
+export default function Tag({ tagList }: Props) {
+    const title = '태그 목록'
+    const contentRef = useRef(null)
 
-//     return (
-//         <div>
-//             <HeadTitle title={title} />
-//             <Navigation contentRef={contentRef} />
-//             <TagHeader />
-//             <div ref={contentRef}>
-//                 <TagList tagList={tagList} />
-//             </div>
-//         </div>
-//     )
-// }
+    useEffect(() => {
 
-// export async function getStaticProps() {
-//     const API_URL = process.env.API
+    },[])
 
-//     // 태그 목록 불러오기
-//     const res: any = await axios.get(`${API_URL}lists/tags`, {
-//         headers: {
-//             'ngrok-skip-browser-warning': '1234',
-//         },
-//     })
+    return (
+        <div>
+            <HeadTitle title={title} />
+            <Navigation contentRef={contentRef} />
+            <TagHeader />
+            <div ref={contentRef}>
+                <TagList tagList={tagList} />
+            </div>
+        </div>
+    )
+}
 
-//     const tagList = res.data
+export async function getStaticProps() {
+    const API_URL = process.env.API
 
-//     // const tagList = ['태그', '뿅', '자바', '자바스크립트', '클라우드', 'Next.js', 'React.js']
+    // 태그 목록 불러오기
+    const res: any = await axios.get(`${API_URL}lists/tags`, {
+        headers: {
+            'ngrok-skip-browser-warning': '1234',
+        },
+    })
 
-//     return {
-//         props: { tagList },
-//         revalidate: 10,
-//     }
-// }
+    const tagList = res.data
+
+    return {
+        props: { tagList },
+        revalidate: 10,
+    }
+}
